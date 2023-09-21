7c693915ce0af30898448ee149bbe1b2e555a70e
Dwain <dwaincontact@gmail.com>
2023-09-21 12:53:52 +0200

    ajout de l'image aléatoire

--- models/jobModel.php
+++ models/jobModel.php
@@ -41,8 +41,8 @@ class JobModel {
     }
 
     public function addJob($data) {
-        $stmt = $this->pdo->prepare("INSERT INTO jobs (publication_date, update_job, reference, title_job, company_name, description_job, location, type_contract, type_job) VALUES (CURDATE(), CURDATE(), :reference, :title, :company_name, :description, :location, :type_contract, :type_job)");
-    
+        $stmt = $this->pdo->prepare("INSERT INTO jobs (publication_date, update_job, reference, title_job, company_name, description_job, location, type_contract, type_job, image_url) VALUES (CURDATE(), CURDATE(), :reference, :title, :company_name, :description, :location, :type_contract, :type_job, :image_url)");
+
         $reference = $this->generateUniqueReference();
     
         $stmt->bindParam(':reference', $reference);
@@ -52,8 +52,23 @@ class JobModel {
         $stmt->bindParam(':location', $data['location']);
         $stmt->bindParam(':type_contract', $data['type_contract']);
         $stmt->bindParam(':type_job', $data['type_job']);
-    
+
+        // Utilisez la fonction getRandomImageUrl pour obtenir une URL d'image aléatoire
+        $image_url = $this->getRandomImageUrl();
+        $stmt->bindParam(':image_url', $image_url);
+
         return $stmt->execute();
+    }
+
+    // Fonction pour obtenir une URL d'image aléatoire de Lorem Picsum
+    private function getRandomImageUrl($width = 300, $height = 200) {
+        $baseUrl = "https://picsum.photos";
+        $url = "{$baseUrl}/{$width}/{$height}";
+
+        // Vous pouvez ajouter d'autres paramètres comme la gravité, la rotation, etc., si nécessaire
+        // Exemple : $url = "{$url}?grayscale&rotate=90";
+
+        return $url;
     }
 
     private function generateUniqueReference() {
@@ -87,10 +102,6 @@ class JobModel {
     
         return $stmt->execute();
     }
-    
-
-
-
 }
 
 ?>
